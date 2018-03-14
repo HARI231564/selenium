4db31876dc47226ab4c71bcce410dea5262ca549
Lavanyaariga <a.kesavasubhash@gmail.com>
2018-03-14 17:48:44 +0530

    Added new DemoClass

---
+++ NewEveningBatch/src/com/icicibank/loans/carloans/DemoClass.java
@@ -0,0 +1,15 @@
+package com.icicibank.loans.carloans;
+
+public class DemoClass 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hi ");
+		System.out.println("hello");
+		System.out.println("welcome");
+
+	}
+
+}
