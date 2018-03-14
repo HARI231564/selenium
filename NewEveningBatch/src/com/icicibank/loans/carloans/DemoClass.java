0549dc0487bec596bfd6917adc46c18705b4ed3d
Lavanyaariga <a.kesavasubhash@gmail.com>
2018-03-14 17:52:50 +0530

    Added adition code in DemoClass

--- NewEveningBatch/src/com/icicibank/loans/carloans/DemoClass.java
+++ NewEveningBatch/src/com/icicibank/loans/carloans/DemoClass.java
@@ -3,12 +3,24 @@ package com.icicibank.loans.carloans;
 public class DemoClass 
 {
 
+	int a=10,b=20,result;
+	
+	public void addition()
+	{
+		result=a+b;
+		System.out.println(result);
+	}
+	
+	
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
 		System.out.println("Hi ");
 		System.out.println("hello");
 		System.out.println("welcome");
+		
+		
+		
 
 	}
 
