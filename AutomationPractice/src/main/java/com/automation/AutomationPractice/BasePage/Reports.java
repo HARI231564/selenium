92bbc9c397da5def30a940a4e44e5dc29738509b
Suuresh <Suuresh@Suuresh-PC>
2018-05-20 13:59:46 +0530

    new

---
+++ AutomationPractice/src/main/java/com/automation/AutomationPractice/BasePage/Reports.java
@@ -0,0 +1,45 @@
+package com.automation.AutomationPractice.BasePage;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import org.openqa.selenium.WebDriver;
+
+import org.testng.annotations.AfterTest;
+
+import org.testng.annotations.BeforeTest;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+
+public class Reports {
+	public static ExtentHtmlReporter htmlreport;
+	public static ExtentReports report;
+	public static ExtentTest logger;
+	public static WebDriver driver = null;
+	protected String timeStamp;
+	String extentReportPath;
+
+	@BeforeTest
+	public void startReport(){
+		
+		timeStamp = new SimpleDateFormat("dd-MMM-yy  hh.mm.ss aa").format(Calendar.getInstance().getTime());
+		extentReportPath = System.getProperty("user.dir") +"//extentReports//"+timeStamp+".html";
+		htmlreport = new ExtentHtmlReporter(extentReportPath);
+		htmlreport.loadXMLConfig(new File(System.getProperty("user.dir")+"\\extent-config.xml"));
+		report = new ExtentReports();
+		report.setSystemInfo("Operating System", "Window_10");
+		report.attachReporter(htmlreport);
+	}
+	
+	
+
+	@AfterTest
+	public void endReport(){
+		report.flush();
+	}
+	
+	
+}
