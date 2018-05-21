aa9fad7df7bd42820d8a4fea77e2b64e9737e152
Lavanyaariga <a.kesavasubhash@gmail.com>
2018-05-22 05:26:12 +0530

    aded new line

--- AutomationPractice/src/main/java/com/automation/AutomationPractice/PageUI/AddToCart.java
+++ AutomationPractice/src/main/java/com/automation/AutomationPractice/PageUI/AddToCart.java
@@ -5,5 +5,6 @@ public class AddToCart
 	public static void main(String[] args) {
 		System.out.println("updated info");
 		System.out.println("second update");
+		System.out.println("third update");
 	}
 }
