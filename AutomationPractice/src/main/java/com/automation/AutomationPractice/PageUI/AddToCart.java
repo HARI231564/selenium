7a63f238ce59faa8fefa842ed5a17b5b2a604296
Lavanyaariga <a.kesavasubhash@gmail.com>
2018-05-22 05:41:04 +0530

    new committe

--- AutomationPractice/src/main/java/com/automation/AutomationPractice/PageUI/AddToCart.java
+++ AutomationPractice/src/main/java/com/automation/AutomationPractice/PageUI/AddToCart.java
@@ -3,9 +3,7 @@ package com.automation.AutomationPractice.PageUI;
 public class AddToCart 
 {
 	public static void main(String[] args) {
-		System.out.println("updated info");
-		System.out.println("second update");
-		System.out.println("third update");
+		
 		
 		
 		System.out.println("updated info");
